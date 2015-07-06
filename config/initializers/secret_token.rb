ec1f13436d65ac1a9416fffd132c012e7663e299
vagrant <leamerluck@gmail.com>
2015-07-06 20:31:36 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Imparter::Application.config.secret_key_base = 'b4a42da5815894347191b008406b04bf6369886fc03248f705f7e62256a30d542b3c89b013ce7899608f217f32bc32e01d735c210507de6b7ee6558bbdd493d1'
