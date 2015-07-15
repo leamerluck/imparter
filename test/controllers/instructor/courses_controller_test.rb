0bfab1ea8b11d680942941c4b1b65efaf6a85c6b
vagrant <leamerluck@gmail.com>
2015-07-15 11:39:55 +0000

    Updated tests

--- test/controllers/instructor/courses_controller_test.rb
+++ test/controllers/instructor/courses_controller_test.rb
@@ -2,11 +2,56 @@ require 'test_helper'
 
 class Instructor::CoursesControllerTest < ActionController::TestCase
 
-   test "new course page loads" do
-   	 FactoryGirl.create(:user)
-   	 current_user
+   test "new course" do
+   	 user = FactoryGirl.create(:user)
+   	 sign_in user
      get :new
      assert_response :success
    end
-   
+
+  test "create course" do 
+   	user = FactoryGirl.create(:user)
+   	sign_in user
+   	
+   	assert_difference "Course.count" do
+	   post :create, {course: {
+   		title: "Yolo",
+   		description: "Easy",
+   		cost: "1"
+   		}
+	   }
+	  end
+
+	  assert_redirected_to instructor_course_path(assigns(:course))
+
+	  assert_equal 1, user.courses.count
+	end
+
+	test "create user not signed in" do 
+		post :create, {course: {
+   		title: "Yolo",
+   		description: "Easy",
+   		cost: "1"
+   		}
+	   }
+
+	  assert_redirected_to new_user_session_path
+	end
+
+	test "create invalid" do
+		user = FactoryGirl.create(:user)
+		sign_in user
+
+		assert_no_difference "Course.count" do
+			post :create, {course: {
+	   		title: "",
+	   		description: "",
+	   		cost: ""
+	   		}
+		   }
+
+		   assert_response :unprocessable_entity
+		end
+	end
+
 end
--- test/factories/courses.rb
+++ test/factories/courses.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
-  #factory :course do
-   # :title "CourseA"
-   # :description "Teaches everything about A"
-   # :cost 98.99
-   # :user_id user_id
-  #end
+  factory :course do
+    title "CourseA"
+    description "Teaches everything about A"
+    cost 98.99
+    association :user
+  end
 end
