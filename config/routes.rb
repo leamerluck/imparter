66ff22ead13526af3e742c5e6cead496cc256f94
vagrant <leamerluck@gmail.com>
2015-09-04 18:54:42 +0000

    Added drag n drop to instructor lessons

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Imparter::Application.routes.draw do
   devise_for :users
   root 'static_pages#index'
-  resources :courses, :only => [:index, :show] do 
-    resources :enrollments, :only => :create 
+  resources :courses, :only => [:index, :show] do
+    resources :enrollments, :only => :create
   end
   resources :lessons, :only => [:show]
   namespace :instructor do
+    resources :lessons, :only => :update
     resources :sections, :only => [] do
       resources :lessons, :only => [:new, :create]
     end
     resources :courses, :only => [:index, :new, :create, :show] do
-      resources :sections, :only => [:new, :create] 
+      resources :sections, :only => [:new, :create]
     end
   end
   # The priority is based upon order of creation: first created -> highest priority.
